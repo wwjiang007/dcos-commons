f4a8c3f3b24f35a26e3c80a195667805f0685642
Jack Harper <jack.harper@couchbase.com>
2017-02-13 15:52:06 -0800

    re-enabling the build test, parameterization complete

--- frameworks/couchbase/src/test/java/com/mesosphere/sdk/couchbase/scheduler/ServiceSpecTest.java
+++ frameworks/couchbase/src/test/java/com/mesosphere/sdk/couchbase/scheduler/ServiceSpecTest.java
@@ -12,17 +12,30 @@ public class ServiceSpecTest extends BaseServiceSpecTest {
         ENV_VARS.set("LIBMESOS_URI", "");
         ENV_VARS.set("PORT_API", "8080");
         ENV_VARS.set("FRAMEWORK_NAME", "couchbase");
-
-        ENV_VARS.set("COUCHBASE_COUNT", "2");
-        ENV_VARS.set("COUCHBASE_CPUS", "0.1");
-        ENV_VARS.set("COUCHBASE_MEM", "512");
-        ENV_VARS.set("COUCHBASE_DISK", "5000");
-
+        ENV_VARS.set("COUCHBASE_VERSION", "4.5.1");
+        ENV_VARS.set("COUCHBASE_INSTALLER_LOCATION", "https://packages.couchbase.com/releases/4.5.1/couchbase-server-enterprise-4.5.1-centos7.x86_64.rpm");
+        ENV_VARS.set("DATA_SERVICE_COUNT", "1");
+        ENV_VARS.set("DATA_SERVICE_CPUS", "4");
+        ENV_VARS.set("DATA_SERVICE_MEM", "4096");
+        ENV_VARS.set("DATA_SERVICE_MEM_USABLE", "3686");
+        ENV_VARS.set("DATA_SERVICE_DISK", "15360");
+        ENV_VARS.set("INDEX_SERVICE_COUNT", "5000");        
+        ENV_VARS.set("INDEX_SERVICE_CPUS", "1");
+        ENV_VARS.set("INDEX_SERVICE_ENABLED","true");
+        ENV_VARS.set("INDEX_SERVICE_INDEX_TYPE", "default");
+        ENV_VARS.set("INDEX_SERVICE_MEM", "4096");
+        ENV_VARS.set("INDEX_SERVICE_MEM_USABLE", "3686");
+        ENV_VARS.set("INDEX_SERVICE_DISK", "15360");
+        ENV_VARS.set("QUERY_SERVICE_ENABLED", "true");
+        ENV_VARS.set("QUERY_SERVICE_COUNT", "1");
+        ENV_VARS.set("QUERY_SERVICE_CPUS", "4");
+        ENV_VARS.set("QUERY_SERVICE_MEM", "4096");
+        ENV_VARS.set("QUERY_SERVICE_DISK", "15360");        
         ENV_VARS.set("SLEEP_DURATION", "1000");
     }
 
     @Test
     public void testYmlBase() throws Exception {
-        // fuck this test testYaml("svc.yml");
+        testYaml("svc.yml");
     }
 }
