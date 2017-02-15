e1c3e2b569e546e4aa42ec82aaac2f4786040e85
Michael Gummelt <mgummelt@mesosphere.io>
2017-02-15 14:23:45 -0800

    upgrade plan

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/DefaultScheduler.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/DefaultScheduler.java
@@ -5,6 +5,8 @@ import com.google.protobuf.TextFormat;
 import com.mesosphere.sdk.scheduler.recovery.RecoveryPlanManagerFactory;
 import com.mesosphere.sdk.scheduler.recovery.constrain.LaunchConstrainer;
 import com.mesosphere.sdk.scheduler.recovery.monitor.FailureMonitor;
+import com.mesosphere.sdk.scheduler.plan.strategy.SerialStrategy;
+import com.mesosphere.sdk.specification.*;
 import com.mesosphere.sdk.specification.yaml.RawServiceSpec;
 import com.mesosphere.sdk.state.*;
 import com.mesosphere.sdk.offer.evaluate.OfferEvaluator;
@@ -36,10 +38,6 @@ import com.mesosphere.sdk.scheduler.recovery.TaskFailureListener;
 import com.mesosphere.sdk.scheduler.recovery.constrain.TimedLaunchConstrainer;
 import com.mesosphere.sdk.scheduler.recovery.monitor.NeverFailureMonitor;
 import com.mesosphere.sdk.scheduler.recovery.monitor.TimedFailureMonitor;
-import com.mesosphere.sdk.specification.DefaultPlanGenerator;
-import com.mesosphere.sdk.specification.DefaultServiceSpec;
-import com.mesosphere.sdk.specification.ReplacementFailurePolicy;
-import com.mesosphere.sdk.specification.ServiceSpec;
 import com.mesosphere.sdk.specification.validation.CapabilityValidator;
 import com.mesosphere.sdk.specification.yaml.RawPlan;
 
@@ -140,7 +138,7 @@ public class DefaultScheduler implements Scheduler, Observer {
         private Builder(ServiceSpec serviceSpec) {
             this.serviceSpec = serviceSpec;
         }
-
+p
         /**
          * Returns the {@link ServiceSpec} which was provided via the constructor.
          */
@@ -572,20 +570,68 @@ public class DefaultScheduler implements Scheduler, Observer {
         Plan deployPlan;
         if (!deploy.isPresent()) {
             LOGGER.info("No deploy plan provided. Generating one");
-             deployPlan = new DefaultPlanFactory(
-                     new DefaultPhaseFactory(new DefaultStepFactory(configStore, stateStore)))
-                     .getPlan(serviceSpec);
+            deployPlan = new DefaultPlanFactory(
+                           new DefaultPhaseFactory(
+                             new DefaultStepFactory(configStore, stateStore)))
+                         .getPlan(serviceSpec);
         } else {
             deployPlan = deploy.get();
         }
         deploymentPlanManager = new DefaultPlanManager(deployPlan);
 
         // All plans are initially created with an interrupted strategy. We generally don't want the deployment plan to
-        // start out interrupted. CanaryStrategy is an exception which explicitly indicates that the deployment plan
-        // should start out interrupted, but CanaryStrategies are only applied to individual Phases, not the Plan as a
+        // getRequirement out interrupted. CanaryStrategy is an exception which explicitly indicates that the deployment plan
+        // should getRequirement out interrupted, but CanaryStrategies are only applied to individual Phases, not the Plan as a
         // whole.
         deploymentPlanManager.getPlan().proceed();
     }
+
+    protected PlanManager getUpgradePlanManager() throws Exception {
+        // get pod types from service spec
+
+        StepFactory stepFactory = new DefaultStepFactory(configStore, stateStore);
+        PodSpec dataPodSpec = serviceSpec.getPods().stream()
+                .filter(podSpec -> podSpec.equals("couchbase_data"))
+                .findAny()
+                .get();
+
+        PodInstance newPodInstance = new DefaultPodInstance(dataPodSpec, dataPodSpec.getCount());
+        PodInstance seedInstance = new DefaultPodInstance(dataPodSpec, 0);
+
+        List<Step> steps = new ArrayList<Step>();
+
+        // add pod N
+        steps.add(stepFactory.getStep(newPodInstance, Collections.singleton("server")));
+        steps.add(stepFactory.getStep(newPodInstance, Collections.singleton("add-data-node")));
+
+        // upgrade pods (N-1)..0
+        for (int i = dataPodSpec.getCount()-1; i >= 0; i--) {
+            PodInstance podInstance = new DefaultPodInstance(dataPodSpec, i);
+            steps.add(stepFactory.getStep(podInstance, Collections.singleton("remove-data-node")));
+
+            Step rebalanceStep = stepFactory.getStep(seedInstance, Collections.singleton("rebalance"));
+            rebalanceStep.setStatus(Status.PENDING);
+            steps.add(rebalanceStep);
+
+            steps.add(stepFactory.getStep(podInstance, Collections.singleton("rm-data")));
+            steps.add(stepFactory.getStep(podInstance, Collections.singleton("server")));
+            steps.add(stepFactory.getStep(podInstance, Collections.singleton("add-data-node")));
+        }
+
+        // remove podN
+        steps.add(stepFactory.getStep(newPodInstance, Collections.singleton("remove-data-node")));
+        steps.add(stepFactory.getStep(newPodInstance, Collections.singleton("rebalance")));
+
+        // TODO: how do we scale down?
+        // 1. unreserve resources
+        // 2. kill task
+        // 3. remove state associated with task
+
+        Phase phase = new DefaultPhase("upgrade", steps, new SerialStrategy<>(), Collections.emptyList());
+        Plan plan = new DefaultPlan("upgrade", Arrays.asList(phase), new SerialStrategy<>());
+        return new DefaultPlanManager(plan);
+    }
+
 
     /**
      * Override this function to inject your own recovery plan manager.
